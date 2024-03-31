ac6bdcdb5938f639ef9ca36e1db55663cd4db2e5
ZainGondal005 <zaingondal005@gmail.com>
2024-03-31 22:58:02 +0500

    WEATHER_API_URL, WEATHER_API_KEY

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 export const geoApiOptions = {
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': '245303d317mshfe8b66bdeeaafd6p1df64cjsn6429c73564f5',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    },
-  };
-  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "245303d317mshfe8b66bdeeaafd6p1df64cjsn6429c73564f5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY='3dda178718544b38b1d922b8fa4f8909'
--- src/compnents/current-weather/current-weather.js
+++ src/compnents/current-weather/current-weather.js
@@ -1,14 +1,38 @@
 import "./current-weather.css";
 
-const currentWeather = () => {
+const currentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Lahore</p>
-          <p className="wather-description">sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="wather-description">{data.weather[0].description}</p>
         </div>
         <img src="icons/01d.png" className="weather-icon" alt="" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>{" "}
+          <div className="parameter-row">
+            <span className="parameter-label">humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>{" "}
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">22 hpa</span>
+          </div>
+        </div>
       </div>
     </div>
   );
