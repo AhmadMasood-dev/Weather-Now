c5c63f526ffb7935f1aaf602c647fe2fa6edf3e8
ZainGondal005 <zaingondal005@gmail.com>
2024-03-31 21:27:08 +0500

    api fetched ad show some cities

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Key': '245303d317mshfe8b66bdeeaafd6p1df64cjsn6429c73564f5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
